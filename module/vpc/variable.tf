98d4577981a130a3c7cbd78929cf6df7769ad087
tarundangeti <tarun.dangeti2@gmail.com>
2026-03-19 00:16:47 +0530

    Create variable.tf

---
+++ module/vpc/variable.tf
@@ -0,0 +1,15 @@
+variable "cidr_block" {
+  description = "this is cidr_block of VPC"
+  type = string
+  default = ""
+}
+variable "az1" {
+  description = "this is az1"
+  type = string
+  default = ""
+}
+variable "az2" {
+  description = "this is az1"
+  type = string
+  default = ""
+}
